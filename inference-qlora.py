b27b6b4e08283bcb6573e77ef55a0543871cce79
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-06-05 13:49:14 +0100

    Update inference-qlora.py

--- inference-qlora.py
+++ inference-qlora.py
@@ -2,18 +2,24 @@
 #pip3 install -U git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git
 
 
+import os
+import bitsandbytes as bnb
+import torch
+from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_id = "Amirkid/Qlora"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+peft_model_id = "Amirkid/LlamaJokeGenerator"
+config = PeftConfig.from_pretrained(peft_model_id)
+model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, return_dict=True, load_in_8bit=True, device_map='auto')
+tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 
-model = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map="auto")
-tokenizer = AutoTokenizer.from_pretrained(model_id)
+# Load the Lora model
+model = PeftModel.from_pretrained(model, peft_model_id)
 
+batch = tokenizer("Amir opened up with this joke", return_tensors='pt')
 
+with torch.cuda.amp.autocast():
+  output_tokens = model.generate(**batch, max_new_tokens=50)
 
-text = "Hello my name is"
-device = "cuda:0"
-
-inputs = tokenizer(text, return_tensors="pt").to(device)
-outputs = model.generate(**inputs, max_new_tokens=20)
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+print('\n\n', tokenizer.decode(output_tokens[0], skip_special_tokens=True))
