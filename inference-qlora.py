4013423d2ea9b58dee6fe1e72e81293407a064df
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-06-05 13:45:21 +0100

    Update inference-qlora.py

--- inference-qlora.py
+++ inference-qlora.py
@@ -1,10 +1,10 @@
-#pip install -U bitsandbytes
-#pip install -U git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git
+#pip3 install -U bitsandbytes scikit-learn
+#pip3 install -U git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git
 
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_id = "facebook/opt-350m"
+model_id = "Amirkid/Qlora"
 
 model = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(model_id)
