776c2b9839f618bebe5338add2dd75b5765ad21d
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-06-05 13:40:51 +0100

    Create inference-qlora.py

---
+++ inference-qlora.py
@@ -0,0 +1,19 @@
+#pip install -U bitsandbytes
+#pip install -U git+https://github.com/huggingface/transformers.git pip install -q -U git+https://github.com/huggingface/accelerate.git pip install -q -U git+https://github.com/huggingface/peft.git
+
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_id = "facebook/opt-350m"
+
+model = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+
+
+text = "Hello my name is"
+device = "cuda:0"
+
+inputs = tokenizer(text, return_tensors="pt").to(device)
+outputs = model.generate(**inputs, max_new_tokens=20)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
