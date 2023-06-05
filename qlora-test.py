6b08880c33ec00d60ced366800b4a3760a37d337
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-06-05 11:55:52 +0100

    Update qlora-test.py

--- qlora-test.py
+++ qlora-test.py
@@ -1,4 +1,4 @@
-#pip3 install -U bitandbytes datasets 
+#pip3 install -U bitandbytes datasets scikit-learn
 #pip3 install -U git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git git+https://github.com/huggingface/transformers.git 
 
 
@@ -15,14 +15,14 @@ from datasets import load_dataset
 model_id = "EleutherAI/gpt-neox-20b"
 bnb_config = BitsAndBytesConfig(load_in_4bit = True,
                                 bnb_4bit_use_double_quant = True,
-                                bnb_4bit_quant_type = "nf4"
+                                bnb_4bit_quant_type = "nf4",
                                 bnb_4bit_compute_dtype = torch.bfloat16
                                 )
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config = bnb_config, device_map = {"": 0})
 
 #Enabling Gradient Checkpointing
-model.gradient_checkpoiting_enable()
+model.gradient_checkpointing_enable()
 model = prepare_model_for_kbit_traning(model)
 
 #Printing the Total Number of trainable parameters
@@ -42,7 +42,7 @@ def print_trainable_parameters(model):
 #Defining the Lora Config
 config = LoraConfig(r = 8,
                     lora_alpha = 32,
-                    target_modules = ['query_key_value']
+                    target_modules = ['query_key_value'],
                     lora_dropout = 0.05,
                     bias = "none",
                     task_type = "CAUSAL_LM"
@@ -63,8 +63,8 @@ tokenizer.pad_token = tokenizer.eos_token
 
 #Initialising the Trainer
 trainer = transformers.Trainer(
-    model = model
-    train_dataset = data["train"]
+    model = model,
+    train_dataset = data["train"],
     args = transformers.TrainingArguments(
         per_device_batch_size =1,
         gradient_accumulation_steps= 4,
@@ -73,9 +73,9 @@ trainer = transformers.Trainer(
         learning_rate = 2e-4,
         fp16 = True,
         logging_steps = 1,
-        output_dir = "outputs"
+        output_dir = "outputs",
         optim = "page_adamw_8bit"
-    )
+    ),
     data_collator = transformers.DataCollatorForLanguageModelling(tokenizer, mlm = False)
     )
 #Use cache or not
