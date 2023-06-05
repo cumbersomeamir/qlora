a0de864953a1b79f2745277b77403a78cfe4bb2b
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-06-05 13:42:54 +0100

    Update inference-qlora.py

--- inference-qlora.py
+++ inference-qlora.py
@@ -1,5 +1,5 @@
 #pip install -U bitsandbytes
-#pip install -U git+https://github.com/huggingface/transformers.git pip install -q -U git+https://github.com/huggingface/accelerate.git pip install -q -U git+https://github.com/huggingface/peft.git
+#pip install -U git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git
 
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
