fc16841e6e7ef6acfca5e08fb1f6a44bca17f77d
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-06-06 20:16:55 +0100

    Create salesbot-llama65B-qlora-inference.py

---
+++ salesbot-llama65B-qlora-inference.py
@@ -0,0 +1,33 @@
+#pip3 install -U bitsandbytes scikit-learn
+#pip3 install -U git+https://github.com/huggingface/transformers.git git+https://github.com/huggingface/accelerate.git git+https://github.com/huggingface/peft.git
+
+import os
+import bitsandbytes as bnb
+import torch
+from peft import PeftModel, PeftConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+peft_model_id = "Amirkid/salesbot-falcon40B-Qlora"
+config = PeftConfig.from_pretrained(peft_model_id)
+model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, return_dict=True, load_in_4bit=True, device_map='auto')
+tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
+
+# Load the Lora model
+model = PeftModel.from_pretrained(model, peft_model_id)
+
+# Loop to keep the conversation going
+while True:
+    # Accept user input
+    user_input = input("You: ")
+
+    # If the user types 'exit', end the conversation
+    if user_input.lower() == 'exit':
+        break
+
+    batch = tokenizer(user_input, return_tensors='pt')
+
+    with torch.cuda.amp.autocast():
+        output_tokens = model.generate(**batch, max_new_tokens=100)
+
+    print('\n\n', tokenizer.decode(output_tokens[0], skip_special_tokens=True))
